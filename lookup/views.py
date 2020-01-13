d74525333ba238656ebb1bb60b15fe8b3161b72f
devanshbajaj <devansh21db.db@gmail.com>
2020-01-13 21:04:58 +0530

    django links,Added API, format API result

--- lookup/views.py
+++ lookup/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+import json
+import requests
 
 def home(request):
-    return render(request, 'home.html', {})
+
+    
+    api_request = requests.get("http://api.openweathermap.org/data/2.5/weather?id=1273293&units=metric&APPID=d0a3abf9e9950b930853deff99063689")
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."   
+
+    return render(request, 'home.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
