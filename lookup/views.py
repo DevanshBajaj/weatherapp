f17c4a9815d684557364ab1e513a52e22adbdcb1
devanshbajaj <devansh21db.db@gmail.com>
2020-01-14 16:41:04 +0530

    nav bar does absolutely nothing

--- lookup/views.py
+++ lookup/views.py
@@ -4,14 +4,26 @@ import requests
 
 def home(request):
 
-    
-    api_request = requests.get("http://api.airvisual.com/v2/nearest_city?key=8b5e9c27-7e2c-4526-9b56-fb94072dd54d")
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error..."   
+    if request.method == "POST":
+        city_name = request.POST['city_name']
+        api_request = requests.get("http://api.airvisual.com/v2/nearest_city?key=8b5e9c27-7e2c-4526-9b56-fb94072dd54d")
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."   
 
-    return render(request, 'home.html', {'api': api})
+        return render(request, 'home.html', {'api': api})
+
+
+
+    else:
+        api_request = requests.get("http://api.airvisual.com/v2/nearest_city?key=8b5e9c27-7e2c-4526-9b56-fb94072dd54d")
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."   
+
+        return render(request, 'home.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
